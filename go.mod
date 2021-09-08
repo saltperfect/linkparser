e3d9fa1a123a0dc6dde2aef3a098ee254d04ddb7
Rupank Pahuja <saltperfect01@gmail.com>
2021-09-08 15:59:23 +0530

    added code whic takes in html and convert to slice of link

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/saltperfect/linkparser
 
 go 1.16
+
+require golang.org/x/net v0.0.0-20210907225631-ff17edfbf26d
---
+++ parse.go
@@ -0,0 +1,64 @@
+package link
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type Link struct {
+	Href string
+	Text string
+}
+
+func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	ret := parseLinks(doc)
+	var links []Link
+	for _, v := range ret {
+		links = append(links, buildLink(v))
+		fmt.Println(v)
+	}
+	return links, nil
+}
+
+func buildLink(node *html.Node ) Link {
+	var ret Link
+	for _, attr := range node.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+			break
+		}
+	}
+	ret.Text = text(node)
+	return ret
+}
+func text(node *html.Node) string {
+	if node.Type == html.TextNode {
+		return node.Data
+	}
+	if node.Type != html.ElementNode {
+		return ""
+	}
+	var ret string
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		ret += text(c)
+	}
+	return strings.Join(strings.Fields(ret), " ")
+}
+
+func parseLinks(n *html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+	var ret []*html.Node
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret = append(ret, parseLinks(c)...)
+	}
+	return ret
+}
